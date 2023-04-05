9db2018206003c8ff5263195f75342d85cd1d747
unknown <nguyendu240901@gmail.com>
2023-04-05 11:54:11 +0700

    add

---
+++ operators/logicalAssignmentOperators.js
@@ -0,0 +1,28 @@
+//! Toán tử gán logic
+// x || y => x || x = y
+// x && y => x && x = y
+// x ?? y => x ?? x = y 
+
+//Or logic: if x is falsy
+let title;
+title ||= 'message';
+console.log(title); //massage
+title = 'Ok';
+title || 'message';
+console.log(title); //Ok
+
+//And logic: if x is truthy
+let age = 10;
+age &&= 21;
+console.log(age); //21
+age = 0;
+age &&= 100;
+console.log(age); //0
+
+//The nullish coalescing assignment operator: if x is null and undefined
+let user = {
+    username: 'NguyenDu',
+};
+user.nickname = 'DuDepTrai';
+
+console.log(user); //{ username: 'NguyenDu', nickname: 'DuDepTrai' }
---
+++ operators/remainderOperator.js
@@ -0,0 +1,46 @@
+let remainder = 5 % -2;
+console.log(remainder); //-1
+
+remainder = 5 % 2;
+console.log(remainder); //1
+
+remainder = -5 % 3;
+console.log(remainder); //-2
+
+remainder = -5 % -3;
+console.log(remainder); //-2
+
+remainder = Infinity % 2;
+console.log(remainder); //NaN
+
+remainder = 10 % 0;
+console.log(remainder); //NaN
+
+console.log(Infinity / Infinity); //NaN
+console.log(Infinity % Infinity); //NaN
+console.log(10 % Infinity); // 10
+console.log(0 % 10); // 0
+console.log(Infinity / 10); //Infinity
+console.log('10' % 3); // 1
+
+//check odd number(kiểm tra số lẻ)
+const num = 13;
+let isOdd = num % 2;
+console.log(Boolean(isOdd)); //true
+
+//function
+function isOddO(num) {
+    return num % 2;
+}
+console.log(Boolean(isOddO(12))); //false
+
+//Arrow function
+const isOddT = (num) => num % 2;
+
+// % not modulo
+// ! modulo javascript: ((dividend % divisor) + divisor) % divisor
+// ? Example: 5, 3
+// todo: ((5 % 3) + 3) % 3 = 2
+
+// function modulo(a, b)
+const mod = (dividend, divisor) =>((dividend % divisor)) % divisor;
